a968f35e69aa486c3df74021a624e4747f6ec907
avbalsam <avbalsam@gmail.com>
2021-09-14 02:40:09 +0300

    added print statement

--- Main.py
+++ Main.py
@@ -58,8 +58,7 @@ def invest(init_length, invest_length, buy_discrepancy, sell_discrepancy):
 
     # After calculating avg_diff, begin investment process
     for x in range(0, invest_length):
-        if x % 20 == 0:
-            print(str(x) + " loops completed. Total profit so far: " + str(total_percent_gain_no_fees))
+        print(str(x) + " loops completed. Total profit so far: " + str(total_percent_gain_no_fees))
         try:
             bid_list = [x.get_bid() for x in exchange_list]
             ask_list = [x.get_ask() for x in exchange_list]
