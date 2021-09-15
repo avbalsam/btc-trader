e0e33e1980bd8634763f4478b0d04bcabdc8c436
avbalsam <avbalsam@gmail.com>
2021-09-15 11:38:11 +0300

    Added pause in between api calls (for bitmex rate limit)

--- Main.py
+++ Main.py
@@ -58,6 +58,7 @@ def invest(init_length, invest_length, buy_discrepancy, sell_discrepancy, verbos
 
     # After calculating avg_diff, begin investment process
     for x in range(0, invest_length):
+        time.sleep(.5)
         if x % 10 == 0:
             print(str(x) + " loops completed. Total profit so far: " + str(total_percent_gain_no_fees))
         try:
