3d76fb2f15eb3431f1703d51a6dd2747673fb69d
avbalsam <avbalsam@gmail.com>
2021-10-10 14:03:30 +0300

    Added logging of investor transactions

--- Main.py
+++ Main.py
@@ -35,24 +35,25 @@ def get_historical_bids(test_length):
     fields = [exchange.name for exchange in exchange_list]
     historical_bids = [list() for i in range(0, len(exchange_list))]
     diff_lists = [list() for i in range(0, len(exchange_list))]
-    avg_diff = [float() for i in range(0, len(exchange_list))]
+    avg_diff = [e.get_bid() - exchange_list[0].get_bid() for e in exchange_list]
     mean_diff = [list() for i in range(0, len(exchange_list))]
-    for x in range(0, test_length):
+    for x in range(1, test_length):
         time.sleep(.05)
         bids = [e.get_bid() for e in exchange_list]
-        print(bid)
-        if x % 1000 == 0 and x != 0:
+        if x % 1000 == 0:
             print(str(x) + " loops completed. Writing collected data to csv...")
             print(bids)
+            print(avg_diff)
             for investor in investors:
                 print(investor.name + " transaction history: " + str(investor.transaction_history))
             write_to_csv("bid_data", fields, historical_bids)
             write_to_csv("diffs_data", fields, diff_lists)
             write_to_csv("mean_diffs_data", fields, mean_diff)
+            write_to_csv("investors_data", investors, [investor.transaction_history for investor in investors])
         for e in range(0, len(exchange_list)):
             historical_bids[e].append(exchange_list[e].get_bid())
             diff_lists[e].append(exchange_list[e].get_bid() - exchange_list[0].get_bid())
-            avg_diff[e] = mean(diff_lists[e])
+            avg_diff[e] = avg_diff[e] * ((x - 1) / x) + diff_lists[e][-1] / x
             mean_diff[e].append(diff_lists[e][-1] - avg_diff[e])
         # TODO Create restart stream method for every exchange and check each one with for loop
         if exchange_list[0].stream_error:
@@ -70,9 +71,9 @@ def get_historical_bids(test_length):
 
 print("Waiting for websockets to connect...")
 while 0.0 in [e.get_bid() for e in exchange_list]:
-    #print([e.get_bid() for e in exchange_list])
+    print([e.get_bid() for e in exchange_list])
     time.sleep(.1)
 
 print("Websockets connected. Starting investment period...")
 
-get_historical_bids(72000)
+get_historical_bids(100000)
