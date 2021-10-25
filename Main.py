bcc5f1224fa80a4349fa9fc253ec57ab4b3cfda6
avbalsam <avbalsam@gmail.com>
2021-10-25 15:24:04 +0300

    Refactor for style

--- Main.py
+++ Main.py
@@ -64,6 +64,8 @@ async def get_market_data():
     avg_diff = [e.get_bid() - exchange_list[0].get_bid() for e in exchange_list]
     mean_diff = [list() for i in range(0, len(exchange_list))]
     await exchange_list[0].update_account_balances()
+    # print(await exchange_list[0].get_profit(Pair('BTC', 'USDT')))
+    # print(len(await exchange_list[0].get_account_trades(Pair('BTC', 'USDT'))))
     x = 0
     while True:
         x += 1
