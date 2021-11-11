7f0b41a51315d633dcb6fe84b55a785cf48ec4ad
avbalsam <avbalsam@gmail.com>
2021-11-11 19:41:40 +0200

    Turn on verbose logging and increase necessary buy discrepancy. Also, add buy discrepancy as an instance variable of Investor class.

--- Main.py
+++ Main.py
@@ -50,7 +50,7 @@ def write_to_csv(filename, fields, data):
 
 
 class Investor:
-    def __init__(self, symbol: str, calibration_time: int, timestep: float, verbose_logging=False, testnet=False):
+    def __init__(self, symbol: str, calibration_time: int, timestep: float, buy_disc: float, verbose_logging=False, testnet=False):
         """Initializes an instance of Investor class
 
         Args:
@@ -59,14 +59,17 @@ class Investor:
             timestep (float): Amount of time in between market data calculations, in seconds.
             verbose_logging (bool): Determines whether the investor will log verbose information.
             testnet (bool): Determines whether to use real money or not.
+            buy_disc (float): Required discrepancy to buy as a percentage of current symbol value (in decimal form)
         """
         self.symbol = symbol
         self.calibration_loops = round(calibration_time / timestep)
+        self.buy_disc = buy_disc
         self.verbose_logging = verbose_logging
         self.exchange_list = [Binance(self, testnet=testnet), AAX(self), Hitbtc(self), KuCoin(self)]
 
         self.sell_order_active = False
         self.buy_order_active = False
+        self.latest_buy_order = None
         self.active_sell_orders = list()
         self.historical_bids = [[e.get_bid(symbol)] for e in self.exchange_list]
         self.fields = [e.name for e in self.exchange_list]
@@ -122,29 +125,29 @@ class Investor:
         self.invest_checks_completed += 1
         buy_disc_count = 0
         sell_disc_count = 0
-        disc_list = [self.exchange_list[e].get_bid(self.symbol) - self.exchange_list[0].get_bid(self.symbol) -
+        disc_list = [self.exchange_list[e].get_ask(self.symbol) - self.exchange_list[0].get_ask(self.symbol) -
                      self.avg_diff[e] for e in range(0, len(self.exchange_list))]
         for d in disc_list:
-            if d > 80:
+            if d > self.exchange_list[0].get_ask(self.symbol) * self.buy_disc:
                 buy_disc_count += 1
             elif d < 0:
                 sell_disc_count += 1
         if self.verbose_logging:
             print(f"{[round(d, 2) for d in disc_list]} {self.invest_checks_completed}")
         if self.loops_completed > self.calibration_loops:
-            if buy_disc_count >= len(self.exchange_list) - 1 and self.exchange_list[0].holdings['USDT'] > 0.001:
+            if buy_disc_count >= len(self.exchange_list) - 1 and self.exchange_list[0].holdings['USDT'] > 50:
                 order_id = await self.exchange_list[0].buy_market(self.symbol)
                 if order_id is not None:
                     self.latest_buy_order = order_id
                     self.buy_order_active = True
-                await asyncio.sleep(.5)
+                await asyncio.sleep(.2)
                 if self.buy_order_active:
                     try:
                         await self.exchange_list[0].cancel_order(self.symbol, self.latest_buy_order)
                     except Exception as e:
                         print(f"Unable to cancel buy order: {e}")
                     self.buy_order_active = False
-        if sell_disc_count >= len(self.exchange_list) - 1:
+        if sell_disc_count >= len(self.exchange_list) - 2:
             order_id = await self.exchange_list[0].sell_market(self.symbol)
             if order_id is not None:
                 self.active_sell_orders.append(order_id)
@@ -175,7 +178,7 @@ async def start_websockets(exchange, loop):
 
 
 if __name__ == "__main__":
-    investor = Investor("BTC", calibration_time=5000, timestep=1, verbose_logging=False, testnet=False)
+    investor = Investor("BTC", calibration_time=5000, timestep=1, buy_disc=0.0013, verbose_logging=True, testnet=False)
     loop = asyncio.get_event_loop()
     results = asyncio.gather(*[start_websockets(e, loop) for e in investor.exchange_list], investor.get_market_data(),
                              investor.cancel_orders())
